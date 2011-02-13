873bcc5a3097ab6bcba40acdf280acab9d874900
wangguan@desktop <netspecters@gmail.com>
2011-02-13 12:31:39 +0800

    implement client

--- src/client.py
+++ src/client.py
@@ -1,27 +1,51 @@
 #coding=utf-8
 
-import threading,socket,ssl,select
+import threading,socket,ssl,struct
 
 class client(threading.Thread):
 
-	def __init__(self,recv_msg_callback,ssl_sock = None,server_address = ('127.0.0.1',51000)): #client will keep ssl_sock
+	MSG_TYPE_HANDSHAKE = 0
+	MSG_TYPE_MESSAGE = 1
+
+	def __init__(self,recv_msg_callback,gui_instance,ssl_sock = None,server_address = ('127.0.0.1',51000)): #client will keep ssl_sock
 		threading.Thread.__init__(self)
 		if ssl_sock is None : #this is a client. In this case, if create client successfully, ssl-tunnel is setuped
 			raw_sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 			raw_sock.connect(server_address)
-			self.ssl_sock = ssl.wrap_socket(raw_sock,ssl_version=ssl.PROTOCOL_TLSv1)
+			self.__ssl_sock = ssl.wrap_socket(raw_sock,ssl_version=ssl.PROTOCOL_TLSv1)
+			self.remote_id,self.remote_nickname = (0,"")
 		else:
-			self.ssl_sock = ssl_sock
+			self.__ssl_sock = ssl_sock
 		self.recv_msg_callback = recv_msg_callback
+		self.gui_instance = gui_instance
 
 	def __del__(self):
-		self.ssl_sock.shutdown(socket.SHUT_RDWR)
-		self.ssl_sock.close()
+		self.__ssl_sock.shutdown(socket.SHUT_RDWR)
+		self.__ssl_sock.close()
+
+	def do_handshake(self,id,nickname):
+		#send handshake
+		msg_header_length = 4*3+len(self.nickname)
+		msg_header_type = client.MSG_TYPE_HANDSHAKE
+		msg_bin = struct.pack("!BBBs",msg_header_length,msg_header_type,id,nickname)
+		self.__ssl_sock.write(msg_bin)
 
 	def send_message(self,message):
-		pass #TODO define the format for message
+		#send message
+		msg_header_length = 4*2+len(message)
+		msg_header_type = client.MSG_TYPE_MESSAGE
+		msg_bin = struct.pack("!BBs",msg_header_length,msg_header_type,message)
+		self.__ssl_sock.write(msg_bin)
 
 	def run(self):
 		while True:
-			pass #TODO add procedure that used for receive message
+			msg_header_bin = self.__ssl_sock.read(struct.calcsize("!BB"))
+			msg_header_length, msg_header_type = struct.unpack("!BB", msg_header_bin)
+			msg_bin = self.__ssl_sock.read(msg_header_length - struct.calcsize("!BB"))
+			if client.MSG_TYPE_HANDSHAKE == msg_header_type:
+				self.remote_id,self.remote_nickname = struct.unpack("!Bs", msg_bin)
+			elif client.MSG_TYPE_MESSAGE == msg_header_type:
+				msg = struct.unpack("!s", msg_bin)
+				self.recv_msg_callback(self,msg,self.gui_instance)
+
 
--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@ import threading,socket,ssl
 import client
 
 class server(threading.Thread):
-	def __init__(self,notify_callback,recv_msg_callback):
+	def __init__(self,gui_instance,notify_callback,recv_msg_callback):
 		threading.Thread.__init__(self)
 		#create raw server socket
 		self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -12,6 +12,7 @@ class server(threading.Thread):
 		self.sock.listen(1)
 		self.running = True
 		self.remote_client_pool = []
+		self.gui_instance = gui_instance
 		self.notify_callback = notify_callback
 		self.recv_msg_callback = recv_msg_callback
 
@@ -24,9 +25,9 @@ class server(threading.Thread):
 			accepted_sock, address = self.sock.accept() #TODO how to use address? Or, is it useful?
 			ssl_sock = ssl.wrap_socket(accepted_sock,server_side=True,ssl_version=ssl.PROTOCOL_TLSv1)
 			try:
-				new_client = client.client(ssl_sock,self.recv_msg_callback)
+				new_client = client.client(ssl_sock,self.recv_msg_callback,self.gui_instance)
 				self.remote_client_pool.append(new_client)
-				self.notify_callback(new_client)
+				self.notify_callback(new_client,self.gui_instance)
 			except:	#May be, the memory has exhausted
 				ssl_sock.shutdown(socket.SHUT_RDWR)
 				ssl_sock.close()
